2921dfc61056f72a0cc21ac8df6cf3e8d1a4b207
jmoore <jmoore@linkedsenior.com>
2014-09-15 15:14:57 -0400

    Initial ant files

---
+++ ant/src/Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+        
+		Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your name and hit return.");
+		
+		String name = in.nextLine();
+
+		System.out.println("Hello " + name);
+    }
+}
